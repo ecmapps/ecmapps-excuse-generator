dcb316ffeea11cdcdb964d0de85380d719997b03
ecmapps <72906286+ecmapps@users.noreply.github.com>
2023-06-01 05:30:13 +0000

    Pending changes exported from your codespace

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
 /* eslint-disable */
-import "bootstrap";
+/*import "bootstrap";*/
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.getElementById("excuse").innerHTML = newExcuse();
 };
+function newExcuse() {
+  let str = "";
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class.",
+    "right on time.",
+    "when I finished.",
+    "during my lunch.",
+    "while I was praying."
+  ];
+  let words = [who, action, what, when];
+  for (let i = 0; i < words.length; i++) {
+    let randomNumber = Math.floor(Math.random() * words[i].length);
+    console.log(
+      words[i] + " the selection from this list is in position " + randomNumber
+    );
+    str += words[i][randomNumber] + randomNumber;
+    if (i + 1 < words.length) {
+      str += " ";
+    }
+  }
+
+  return str;
+}
